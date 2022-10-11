fa0839375f5253da7562b03fbffd330a7b54df10
kelvin-kariuki <Kariuki405@gmail.com>
2022-10-11 14:37:49 +0300

    DOne

---
+++ db/migrate/20210506153127_create_rides.rb
@@ -0,0 +1,10 @@
+class CreateRides < ActiveRecord::Migration[6.1]
+  def change
+    create_table :rides do |t|
+        t.belongs_to :passenger
+      t.belongs_to :taxi
+
+      t.timestamps
+    end
+  end
+end
